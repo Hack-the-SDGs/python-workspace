80e5982480d514c7839f4d0a75f14aa4ac3c64aa
xinshoutw <me@xinshou.tw>
2026-07-02 06:19:43 +0800

    init: init beginner workspace

---
+++ L1_python/C1_whoami.py
@@ -0,0 +1,6 @@
+import socket
+
+hostname = socket.gethostname()
+
+print(f"歡迎來到 Hack-the-SDGs !!!")
+print(f"現在這臺電腦是：{hostname}")
---
+++ L2_minethon/C1_join.py
@@ -0,0 +1,4 @@
+from minethon import create_bot
+
+bot = create_bot("bot")
+bot.chat("哈囉大家好")
